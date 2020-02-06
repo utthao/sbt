610179d687dda8dfbfd6de6f928b42ad56c059ec
PAT <phamanhtai263@gmail.com>
2020-02-06 20:10:23 +0700

    Revenue

--- app/controllers/revenue_controller.rb
+++ app/controllers/revenue_controller.rb
@@ -1,18 +1,22 @@
 class RevenueController < ApplicationController
 
   def index
+    @revenue = 0
   end
 
   def create
     revenue_month = params[:month].to_i
     done_bookings = Booking.where(status: "accepted")
-    revenue = 0
+    @count_done_bookings = 0
+    @revenue = 0
     done_bookings.each do |done_booking|
       if done_booking.tour.start_day.to_i <= Time.now.to_i && done_booking.tour.start_day.to_i >= revenue_month.months.ago.to_i
-        revenue +=done_booking.total_price
+        @revenue +=done_booking.total_price
+        @count_done_bookings += 1
       end
     end
-    byebug
-    redirect revenue_index_path
+    respond_to do |format|
+      format.js
+    end
   end
 end
