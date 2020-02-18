0f9112eb53148cef0bcf19e3a686f8adfb0468a1
utthao <thaoduyendao98@gmail.com>
2020-02-18 10:30:40 +0700

    final_pr2

--- app/helpers/accounts_helper.rb
+++ app/helpers/accounts_helper.rb
@@ -25,4 +25,11 @@ module AccountsHelper
       redirect_to root_url
     end
   end
+
+  def gravatar_for(account,  options = { size: 80 })
+    gravatar_id = Digest::MD5::hexdigest(account.email.downcase)
+    size = options[:size]
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image_tag(gravatar_url, alt: account.name, class: "gravatar")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
-  # match '/404', to: 'error/errors#not_found', via: :all
-  # match '/500', to: 'error/errors#internal_server_error', via: :all
   get "users/new"
   root "static_pages#home"
   get "/donetours", to: "static_pages#donetours"
   get "/help", to: "static_pages#help"
   get "/search", to: "tours#search"
   post "/search", to: "tours#search"
+  get "/profile", to: "accounts#show"
   resources :sessions
   resources :tours
   resources :user_bookings
@@ -23,5 +22,4 @@ Rails.application.routes.draw do
   end
   devise_for :accounts, :controllers => {:sessions => "sessions"}
   resources :accounts
-  # mount Sidekiq::Web, at: "/sidekiq"
 end
